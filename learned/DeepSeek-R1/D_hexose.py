caf7273d41877a06d7757cfee1089b13f32134cc
dragon-ai-agent <cmungall+dragon-ai-agent@gmail.com>
2025-02-17 19:03:49 -0800

    Attempt 1 at D-hexose, f1=0.0
    (currently fails to compile, but committing anyway
    
    None
    # INFORMATION:
    
    ---
    chemical_class:
      id: CHEBI:4194
      name: D-hexose
      definition: A hexose that has D-configuration at position 5.
      parents:
      - CHEBI:18133
      xrefs:
      - KEGG:C00738
      all_positive_examples: []
    config: null
    code_statistics:
      lines_of_code: 0
      log_lines_of_code: 0.0
      max_indent: 0
      imports: []
      imports_count: 0
      methods_called: []
      methods_called_count: 0
      smarts_strings: []
      smarts_strings_count: 0
      defs: []
      defs_count: 0
      returns: []
      returns_count: 0
      complexity: 0.0
    true_negatives: null
    false_negatives: null
    sample_true_negatives: null
    sample_false_negatives: null
    attempt: 0
    success: false
    best: false
    error: unexpected indent (<string>, line 1)
    stdout: ''
    num_true_positives: 0
    num_false_positives: 0
    num_true_negatives: 0
    num_false_negatives: 0
    num_negatives: null
    precision: 0.0
    recall: 0.0
    f1: 0.0
    accuracy: null
    negative_predictive_value: 0.0
    sample_true_positives: []

---
+++ learned/DeepSeek-R1/D_hexose.py
@@ -0,0 +1,21 @@
+"""
+Classifies: CHEBI:4194 D-hexose
+"""
+ - alpha-D-altrose: SMILES: OC[C@H]1O[C@H](O)[C@H](O)[C@H](O)[C@H]1O
+ - D-galactopyranose: SMILES: OC[C@H]1OC(O)[C@H](O)[C@@H](O)[C@H]1O
+ - D-mannofuranose: SMILES: O1[C@H]([C@@H](O)[C@H](O)C1O)[C@H](O)CO
+ - beta-D-galactopyranose: SMILES: OC[C@H]1O[C@@H](O)[C@H](O)[C@@H](O)[C@H]1O
+ - D-altropyranose: SMILES: OC[C@H]1OC(O)[C@H](O)[C@H](O)[C@H]1O
+ - alpha-D-idose: SMILES: OC[C@H]1O[C@H](O)[C@@H](O)[C@H](O)[C@H]1O
+ - beta-D-allopyranose: SMILES: OC[C@H]1O[C@@H](O)[C@H](O)[C@H](O)[C@@H]1O
+ - beta-D-talose: SMILES: OC[C@H]1O[C@@H](O)[C@@H](O)[C@@H](O)[C@H]1O
+ - D-gulofuranose: SMILES: O1[C@H]([C@@H](O)[C@H](O)C1O)[C@H](O)CO
+ - D-psicopyranose: SMILES: OC[C@H]1OC(O)[C@H](O)[C@H](O)[C@H]1O
+ - D-sorbofuranose: SMILES: O1[C@H]([C@@H](O)[C@H](O)C1O)[C@H](O)CO
+ - alpha-D-psicofuranose: SMILES: O1[C@@H]([C@H](O)[C@@H](O)[C@H]1O)[C@H](O)CO
+ - alpha-D-sorbofuranose: SMILES: O1[C@@H]([C@H](O)[C@@H](O)[C@H]1O)[C@H](O)CO
+ - alpha-D-fructopyranose: SMILES: O1[C@H]([C@H](O)[C@H](O)[C@H]1O)[C@H](O)CO
+ - beta-D-fructofuranose: SMILES: O1[C@H]([C@@H](O)[C@H](O)[C@H]1O)[C@H](O)CO
+ - alpha-D-tagatofuranose: SMILES: O1[C@H]([C@H](O)[C@H](O)[C@H]1O)[C@H](O)CO
+ - beta-D-tagatopyranose: SMILES: OC[C@H]1O[C@@H](O)[C@H](O)[C@H](O)[C@H]1O
+ - beta-D-psicofuranose: SMILES: O1[C@H]([C@@H](O)[
