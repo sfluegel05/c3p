7d7f962ba71b8e03b9b9b833e10aa1da38d87fc9
dragon-ai-agent <cmungall+dragon-ai-agent@gmail.com>
2025-02-17 19:05:11 -0800

    Attempt 1 at organometalloidal compound, f1=0.0
    (currently fails to compile, but committing anyway
    
    None
    # INFORMATION:
    
    ---
    chemical_class:
      id: CHEBI:143084
      name: organometalloidal compound
      definition: A compound having bonds between one or more metalloid atoms and one
        or more carbon atoms of an organyl group.
      parents:
      - CHEBI:50860
      xrefs:
      - PMID:15246002
      - PMID:17741021
      - PMID:8403081
      - PMID:908316
      all_positive_examples: []
    config: null
    code_statistics:
      lines_of_code: 0
      log_lines_of_code: 0.0
      max_indent: 0
      imports: []
      imports_count: 0
      methods_called: []
      methods_called_count: 0
      smarts_strings: []
      smarts_strings_count: 0
      defs: []
      defs_count: 0
      returns: []
      returns_count: 0
      complexity: 0.0
    true_negatives: null
    false_negatives: null
    sample_true_negatives: null
    sample_false_negatives: null
    attempt: 0
    success: false
    best: false
    error: unexpected indent (<string>, line 1)
    stdout: ''
    num_true_positives: 0
    num_false_positives: 0
    num_true_negatives: 0
    num_false_negatives: 0
    num_negatives: null
    precision: 0.0
    recall: 0.0
    f1: 0.0
    accuracy: null
    negative_predictive_value: 0.0
    sample_true_positives: []

---
+++ learned/DeepSeek-R1/organometalloidal_compound.py
@@ -0,0 +1,6 @@
+"""
+Classifies: CHEBI:143084 organometalloidal compound
+"""
+ - arsonocholine: SMILES: O[As](O)(=O)CC[N+](C)(C)C
+ - 1-dimethylarsinoyl-(3Z,6Z,9Z,12Z,15Z,18Z)-nonadecahexaene: SMILES: [As](=O)(CC/C=C\C/C=C\C/C=C\C/C=C\C/C=C\C/C=C\C)(C)C
+ - 1-dimethylarsinoyl-(3Z,6Z,9Z,12Z,15Z)-octadecapentaene: SMILES: [As](=O)(CC/C=C\C/C=C\C/C=C\C/C=C\C
