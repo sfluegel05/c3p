0216a4ce1098aa93aff9dfc47fb89f3df0e273fb
dragon-ai-agent <cmungall+dragon-ai-agent@gmail.com>
2025-02-17 19:05:08 -0800

    Attempt 1 at polypyrrole, f1=0.0
    (currently fails to compile, but committing anyway
    
    None
    # INFORMATION:
    
    ---
    chemical_class:
      id: CHEBI:38077
      name: polypyrrole
      definition: A compound composed of two or more pyrrole units.
      parents:
      - CHEBI:38101
      xrefs:
      - Beilstein:8538310
      all_positive_examples: []
    config: null
    code_statistics:
      lines_of_code: 0
      log_lines_of_code: 0.0
      max_indent: 0
      imports: []
      imports_count: 0
      methods_called: []
      methods_called_count: 0
      smarts_strings: []
      smarts_strings_count: 0
      defs: []
      defs_count: 0
      returns: []
      returns_count: 0
      complexity: 0.0
    true_negatives: null
    false_negatives: null
    sample_true_negatives: null
    sample_false_negatives: null
    attempt: 0
    success: false
    best: false
    error: unexpected indent (<string>, line 1)
    stdout: ''
    num_true_positives: 0
    num_false_positives: 0
    num_true_negatives: 0
    num_false_negatives: 0
    num_negatives: null
    precision: 0.0
    recall: 0.0
    f1: 0.0
    accuracy: null
    negative_predictive_value: 0.0
    sample_true_positives: []

---
+++ learned/DeepSeek-R1/polypyrrole.py
@@ -0,0 +1,9 @@
+"""
+Classifies: CHEBI:38077 polypyrrole
+"""
+ - pheophorbide a: SMILES: CCC1=C(C)C2=Cc3c(C=C)c(C)c4C=C5[C@@H](C)[C@H](CCC(=O)OC\C=C(/C)CCC[C@H](C)CCC[C@H](C)CCCC(C)C)C6=[N+]5[Mg--]5(n34)n3c(=CC1=[N+]25)c(C)c1C(=O)[C@H](C(=O)OC)C6=c31
+ - (3Z)-phycoerythrobilin: SMILES: C(\C=1C(=C(\C(\N1)=C\C=2NC(=C(C2CCC(O)=O)C)/C=C/3\NC([C@@H](\C3=C\C)C)=O)CCC(=O)O)C)=C/4\NC(C(=C4C)C=C)=O
+ - (2E)-phytochromobilin: SMILES: C(\C=1C(=C(\C(\N1)=C\C=2NC(=C(C2CCC(O)=O)C)/C=C/3\NC([C@@H](C3=C(C)C)C)=O)CCC(=O)O)C)=C/4\NC(C(=C4C)C=C)=O
+ - pheophytin a: SMILES: CCC1=C(C)C2=Cc3c(C=C)c(C)c4C=C5[C@@H](C)[C@H](CCC(=O)OC\C=C(/C)CCC[C@H](C)CCC[C@H](C)CCCC(C)C)C6=[N+]5[Mg--]5(n34)n3c(=CC1=[N+]25)c(C)c1C(=O)[C@H](C(=O)OC)C6=c31
+ - pheophytin b: SMILES: CC(C=O)C1=C(C)C2=Cc3c(C=C)c(C)c4C=C5[C@@H](C)[C@H](CCC(=O)OC\C=C(/C)CCC[C@H](C)CCC[C@H](C)CCCC(C)C)C6=[N+]5[Mg--]5(n34)n3c(=CC1=[N+]25)c(C)c1C(=O)[C@H](C(=O)OC)C6=c31
+ - 2,3,7,8,12,13,17,18-octaethylporphyrin: SMILES: CC1=C(CC)C2=CC3=C(CC)C(=C(C=C4N=C(C(=C4
