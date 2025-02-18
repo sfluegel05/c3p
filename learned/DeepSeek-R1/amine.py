65def7823aea2a6efccef708e3b5cc9349db6c3d
dragon-ai-agent <cmungall+dragon-ai-agent@gmail.com>
2025-02-17 19:04:49 -0800

    Attempt 1 at amine, f1=0.0
    (currently fails to compile, but committing anyway
    
    None
    # INFORMATION:
    
    ---
    chemical_class:
      id: CHEBI:32952
      name: amine
      definition: A compound formally derived from ammonia by replacing one, two or three
        hydrogen atoms by hydrocarbyl groups.
      parents:
      - CHEBI:50047
      xrefs:
      - KEGG:C00706
      all_positive_examples: []
    config: null
    code_statistics:
      lines_of_code: 0
      log_lines_of_code: 0.0
      max_indent: 0
      imports: []
      imports_count: 0
      methods_called: []
      methods_called_count: 0
      smarts_strings: []
      smarts_strings_count: 0
      defs: []
      defs_count: 0
      returns: []
      returns_count: 0
      complexity: 0.0
    true_negatives: null
    false_negatives: null
    sample_true_negatives: null
    sample_false_negatives: null
    attempt: 0
    success: false
    best: false
    error: unexpected indent (<string>, line 1)
    stdout: ''
    num_true_positives: 0
    num_false_positives: 0
    num_true_negatives: 0
    num_false_negatives: 0
    num_negatives: null
    precision: 0.0
    recall: 0.0
    f1: 0.0
    accuracy: null
    negative_predictive_value: 0.0
    sample_true_positives: []

---
+++ learned/DeepSeek-R1/amine.py
@@ -0,0 +1,8 @@
+"""
+Classifies: CHEBI:32952 amine
+"""
+ - 2-[[4-(4-fluorophenyl)-1-piperazinyl]methyl]-1H-isoindole-1,3(2H)-dione: SMILES: O=C1N(C(CN2CCN(CC2)C3=CC=C(C=C3)F)C2=CC=CC=C2)C(=O)C1
+ - (S)-3-aminobutanoic acid: SMILES: N[C@@H](CC(=O)O)C
+ - 3,4-Dimethoxyamphetamine: SMILES: COC1=C(CC(N)C)C=C(OC)C=C1
+ - 2,5-Dimethoxyamphetamine: SMILES: COC1=CC(=C(CC(N)C)C=C1)OC
+ - 3-(3,4
